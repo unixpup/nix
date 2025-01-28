a5758b42e34a3c652a8c7c356bd67622c6e125c9
unixpup <dev@fault.wtf>
2025-01-27 22:46:14 -0300

    Basic hardening

--- hosts/mainframe/boot.nix
+++ hosts/mainframe/boot.nix
@@ -1,5 +1,17 @@
 {pkgs, ...}: {
+  services.btrfs.autoScrub = {
+    fileSystems = ["/"]; # Assuming root is formatted with Btrfs.
+    interval = "daily";
+    enable = true;
+  };
+
   hardware = {
+    cpu.amd = {
+      sev.enable = true;
+      updateMicrocode = true;
+      ryzen-smu.enable = true;
+    };
+    cpu.x86.msr.enable = true;
     amdgpu = {
       initrd = {
         enable = true;
@@ -11,6 +23,23 @@
     };
   };
   boot = {
+    kernelParams = [
+      "default_hugepagesz=1G"
+      "hugepagesz=1G"
+      "slab_nomerge"
+      "init_on_alloc=1"
+      "randomize_kstack_offset=on"
+      "init_on_free=1"
+      "page_alloc.shuffle=1"
+      "pti=on"
+      "vsyscall=none"
+      "oops=panic"
+      "module.sig_enforce=1"
+      "mce=0"
+      "quiet"
+      "splash"
+      "loglevel=0"
+    ];
     kernelPackages = pkgs.linuxPackages_cachyos;
     initrd = {
       systemd = {
