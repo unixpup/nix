b4da527bb23546051983702717c29dfd68adc126
unixpup <dev@fault.wtf>
2025-01-27 22:17:39 -0300

    Nyx

--- flake.nix
+++ flake.nix
@@ -2,16 +2,24 @@
   description = "boiiii watchu say abt nixos";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
 
   outputs = {
     self,
     nixpkgs,
+    chaotic,
+    ...
   }: {
-    nixosConfigurations.mainframe = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [./hosts/mainframe/default.nix];
+    nixosConfigurations = {
+      mainframe = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/mainframe/default.nix
+          chaotic.nixosModules.default
+        ];
+      };
     };
   };
 }
